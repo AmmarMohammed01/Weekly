c9f577f92f5db9a95aa080b19fde06932eb4395f
Ammar Mohammed <ammar.hm321@gmail.com>
2023-10-09 22:43:03 -0500

    Version now has 7 days

--- Weekly.js
+++ Weekly.js
@@ -1,17 +1,21 @@
 let grandArr = JSON.parse(localStorage.getItem('grandArr'));
-let monArr = grandArr[0];
-let tueArr = grandArr[1];
-let wedArr = grandArr[2];
-let thurArr = grandArr[3];
-let friArr = grandArr[4];
+let sunArr = grandArr[0];
+let monArr = grandArr[1];
+let tueArr = grandArr[2];
+let wedArr = grandArr[3];
+let thurArr = grandArr[4];
+let friArr = grandArr[5];
+let satArr = grandArr[6];
 
 if(!grandArr) {
   grandArr = [];
+  sunArr = [];
   monArr = [];
   tueArr = [];
   wedArr = [];
   thurArr = [];
   friArr = [];
+  satArr = [];
 }
 else {
   renderAll();
@@ -26,7 +30,12 @@ function addTodo(day) {
     return;
   }
 
-  if(day === 'monday') { //specific
+  if(day === 'sunday') { //specific
+    sunArr.push(name); //sp
+    inputElement.value = '';
+    renderTodoList(sunArr); //sp
+  } 
+  else if(day === 'monday') { //specific
     monArr.push(name); //sp
     inputElement.value = '';
     renderTodoList(monArr); //sp
@@ -51,6 +60,11 @@ function addTodo(day) {
     inputElement.value = '';
     renderTodoList(friArr);
   }
+  else if (day === 'saturday') {
+    satArr.push(name);
+    inputElement.value = '';
+    renderTodoList(satArr);
+  }
 }
 
 function renderTodoList(dayList) {
@@ -62,28 +76,36 @@ function renderTodoList(dayList) {
     todoHTML += html;
   }
 
-  if(dayList === monArr) { //sp
+  if(dayList === sunArr) { //sp
     document.querySelector('.js-output1').innerHTML = todoHTML; //sp
     console.log(todoHTML);
   }
-  else if (dayList === tueArr) {
-    document.querySelector('.js-output2').innerHTML = todoHTML;
+  else if(dayList === monArr) { //sp
+    document.querySelector('.js-output2').innerHTML = todoHTML; //sp
     console.log(todoHTML);
   }
-  else if (dayList === wedArr) {
+  else if (dayList === tueArr) {
     document.querySelector('.js-output3').innerHTML = todoHTML;
     console.log(todoHTML);
   }
-  else if (dayList === thurArr) {
+  else if (dayList === wedArr) {
     document.querySelector('.js-output4').innerHTML = todoHTML;
     console.log(todoHTML);
   }
-  else if (dayList === friArr) {
+  else if (dayList === thurArr) {
     document.querySelector('.js-output5').innerHTML = todoHTML;
     console.log(todoHTML);
   }
+  else if (dayList === friArr) {
+    document.querySelector('.js-output6').innerHTML = todoHTML;
+    console.log(todoHTML);
+  }
+  else if (dayList === satArr) {
+    document.querySelector('.js-output7').innerHTML = todoHTML;
+    console.log(todoHTML);
+  }
 
-  grandArr = [monArr, tueArr, wedArr, thurArr, friArr];
+  grandArr = [sunArr, monArr, tueArr, wedArr, thurArr, friArr, satArr];
   console.log(grandArr);
   //console.log(JSON.stringify(grandArr));
   localStorage.setItem('grandArr', JSON.stringify(grandArr));
@@ -91,20 +113,24 @@ function renderTodoList(dayList) {
 
 function renderAll() {
   console.log('running renderAll()');
+  renderTodoList(sunArr);
   renderTodoList(monArr);
   renderTodoList(tueArr);
   renderTodoList(wedArr);
   renderTodoList(thurArr);
   renderTodoList(friArr);
+  renderTodoList(satArr);
 }
 
 function clearAll() {
   grandArr = [];
+  sunArr = [];
   monArr = [];
   tueArr = [];
   wedArr = [];
   thurArr = [];
   friArr = [];
+  satArr = [];
   localStorage.setItem('grandArr', JSON.stringify(grandArr));
   renderAll();
 }
