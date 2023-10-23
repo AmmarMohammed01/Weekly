1806f046e087113b8555afff375a05bd0d929612
Ammar Mohammed <ammar.hm321@gmail.com>
2023-10-23 10:08:39 -0500

    day buttons also change in dark mode

--- DarkLight.js
+++ DarkLight.js
@@ -2,6 +2,7 @@ let webView = false;
 //This controls whether the user see website in light or dark mode
 function webpageView() {
   const switchDayBox = document.querySelectorAll(".day-container");
+  const switchDayButton = document.querySelectorAll('.button-day')
 
   //if currently not dark mode
   if(webView === false) {
@@ -16,12 +17,17 @@ function webpageView() {
     for(let i = 0; i < switchDayBox.length; i++) {
       //switchDayBox[i].style["border-color"] = 'white';
       switchDayBox[i].style.color = 'white';
+
+      //change buttons for day
+      switchDayButton[i].style['background-color'] = 'white';
+      switchDayButton[i].style.color = 'black';
     }
 
     //change button dark/light mode button color
     document.querySelector('.button-view').style['background-color'] = 'white';
     document.querySelector('.button-view').style.color = 'black';
     document.querySelector('.button-view').innerHTML = 'Light Mode';
+
   }
   else {
     webView = false;
@@ -33,6 +39,10 @@ function webpageView() {
     for(let i = 0; i < switchDayBox.length; i++) {
       //switchDayBox[i].style["border-color"] = 'black';
       switchDayBox[i].style.color = 'black';
+
+      //change buttons for day
+      switchDayButton[i].style['background-color'] = 'black';
+      switchDayButton[i].style.color = 'white';
     }
 
     document.querySelector('.button-view').style['background-color'] = 'black';
