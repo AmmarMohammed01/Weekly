12a79d2ff3f0d6066103def38837671051787fbf
Ammar Mohammed <ammar.hm321@gmail.com>
2024-12-17 20:02:23 -0600

    Added complete task feature

--- Weekly2.js
+++ Weekly2.js
@@ -71,14 +71,24 @@ function renderTasks() {
   }
 
   taskList.forEach((task) => {
+    checkBtnHTML = task.isComplete ? `<button class="js-check-button css2-checkbtn-complete" data-task-id=${task.taskId}>Check</button>` : `<button class="js-check-button" data-task-id=${task.taskId}>Check</button>`
+
     document.querySelector(`.js-output-${task.day}`).innerHTML += `
     <div class="js-task-box-${task.taskId}">
       ${task.taskName} 
-      <button>Check</button>
+      ${checkBtnHTML}
       <button>Edit</button>
       <button class="js-delete-button" data-task-id="${task.taskId}">Delete</button>
     </div>
     `;
+  });
+
+  const checkButtons = document.querySelectorAll('.js-check-button');
+  checkButtons.forEach( (button) => {
+    button.addEventListener('click', () => {
+      const {taskId} = button.dataset;
+      completeTask(taskId);
+    });
   });
 
   const deleteButtons = document.querySelectorAll('.js-delete-button');
@@ -94,12 +104,8 @@ renderTasks();
 
 function deleteTask(deleteId) {
   taskList.forEach((task, index) => {
-    console.log(`hi${index}`);
-    console.log(`task.taskId: ${task.taskId} and deleteId: ${deleteId}`);
-    console.log(task.taskId === Number(deleteId));
-    if(task.taskId === Number(deleteId)) {
+    if(task.taskId === Number(deleteId)) { //I was having a few errors here because deleteId was a string
       taskList.splice(index, 1);
-      console.log(JSON.stringify(taskList));
       saveData();
     }
   });
@@ -108,4 +114,14 @@ function deleteTask(deleteId) {
   modifiedSelector = selector.replace('.', '\\.');
 
   document.querySelector(`.js-task-box-${modifiedSelector}`).remove();
+}
+
+function completeTask(completeId) {
+  taskList.forEach((task, index) => {
+    if(task.taskId === Number(completeId)) { 
+      taskList[index].isComplete = !taskList[index].isComplete ? true : false;
+      saveData();
+      renderTasks(); //I feel like there is a better way of doing this
+    }
+  });
 }
